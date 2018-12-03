dd530c70404f0870eff5b87431eb3cb0a66bcb4e
Nanban <dminpo@gmail.com>
2018-12-03 19:16:16 +0900

    aa
    aa

---
+++ じーぴーゆーなんたらんしんｇ/Assets/Shader/TestC.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestC : MonoBehaviour {
+
+    public ComputeShader shader;
+
+	// Use this for initialization
+	void Start () {
+        ComputeBuffer result = new ComputeBuffer(4, sizeof(int));
+        shader.SetBuffer(0, "Result", result);
+
+        int kernel = shader.FindKernel("CSMain");
+
+        shader.Dispatch(kernel, 1, 1, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
